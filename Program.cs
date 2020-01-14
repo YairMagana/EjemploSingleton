a562c76a639cbc63885973d3fe7ccf6f943313e6
Yair Jafet Magaña Chan <yair.magana@blueoceantech.com.mx>
2020-01-14 08:04:48 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.ObtenerInstancia();
+            Singleton s2 = Singleton.ObtenerInstancia();
+
+            string resultado;
+
+            if (s1 == s2)
+            {
+                resultado = "Ambas instancias son la misma.";
+            }
+            else
+            {
+                resultado = "Son diferentes instancias.";
+            }
+
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,17 @@
+﻿namespace EjemploSingleton
+{
+    class Singleton
+    {
+        private static Singleton instancia;
+
+        private Singleton() { }
+
+        public static Singleton ObtenerInstancia()
+        {
+            if (instancia == null)
+                instancia = new Singleton();
+
+            return instancia;
+        }
+    }
+}
